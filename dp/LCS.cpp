8b3f45ff9b88772f6dabdba08891b8ed81677101
ritwick-roy <ritwickroy2002@gmail.com>
2023-06-07 08:27:19 +0530

    added LCS

---
+++ dp/LCS.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int func(vector<vector<int> >& dp,string& a,string& b,int i,int j)
+{
+  if(i<0||j<0) return 0;
+  if(dp[i][j]!=-1)
+  return dp[i][j];
+  if(a[i]==b[j])
+  return dp[i][j]=1+func(dp,a,b,i-1,j-1);
+  return dp[i][j]=max(func(dp,a,b,i-1,j),func(dp,a,b,i,j-1));
+}
+int longestCommonSubsequence(string a, string b)
+{
+  int i,j,m=a.size(),n=b.size();
+  vector<vector<int> > dp(m+1,vector<int>(n+1,0));
+  for(i=1;i<=m;++i)
+  {
+    for(j=1;j<=n;++j)
+    {
+      if(a[i-1]==b[j-1])
+        dp[i][j]=1+dp[i-1][j-1];
+      else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    }
+  }
+  return dp[m][n];
+}
